12ea2fc39d2b7980c23eb7824561c633fef8d578
alexagold1 <526agold@frhsd.com>
2024-12-02 12:00:13 -0500

    changed name of images

--- script.js
+++ script.js
@@ -14,40 +14,40 @@ function shakeMagic8Ball(){
     switch (randomNumber) {
       case 0:
         eightBall = 'It is certain';
-        responseImg = "it is certain.jpg";
+        responseImg = "itiscertain.jpg";
         break;
       case 1:
         eightBall = 'It is decidedly so';
-        responseImg = "it is decidedly so.jpg";
+        responseImg = "itisdecidedlyso.jpg";
         break;
       case 2:
         eightBall = 'Focus and ask again';
-        responseImg = "focus and ask again.png";
+        responseImg = "focusandaskagain.png";
         break;
       case 3:
         eightBall = 'Cannot predict now';
-        responseImg = "cannot predict now.png";
+        responseImg = "cannotpredictnow.png";
         break;
       case 4:
         eightBall = 'Do not count on it';
-        responseImg = "dont count on it.png";
+        responseImg = "dontcountonit.png";
         break;
       case 5:
         eightBall = 'My sources say no';
-        responseImg = "my sources say no.png";
+        responseImg = "mysourcessayno.png";
         break;
       case 6:
         eightBall = 'Outlook not so good';
-        responseImg = "outlook not so good";
+        responseImg = "outlooknotsogood.png";
         break;
       case 7:
         eightBall = 'Signs point to yes';
-        responseImg = "signs point to yes good.png";
+        responseImg = "signspointtoyesgood.png";
         break;
     }
 
 console.log(eightBall)
-document.getElementById('response-text').innerText = `${userName} has asked the Magic 8 Ball ${userQuestion}. The wise and wondrous 8 ball has replied ${eightBall}`
+document.getElementById('response-text').innerText = `${userName} has asked the Magic 8 Ball ${userQuestion}. The wise and wondrous 8 ball has replied ${eightBall}`;
 
-document.getElementById("response-image").src = responseImg;
+document.getElementById('response-image').src = responseImg;
 }
